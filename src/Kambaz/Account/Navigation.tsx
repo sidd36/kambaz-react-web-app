4bb97b95d502f6a320e13c6a367536a392628823
siddharth_sanjeev <sanjeev.si@northeastern.edu>
2025-04-16 12:59:10 -0400

    Hw6 commit 1

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -4,6 +4,7 @@ export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "text-danger");
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       {links.includes("Signin") && <><Link to={`/Kambaz/Account/Signin`}
@@ -12,6 +13,8 @@ export default function AccountNavigation() {
         className={`list-group-item border border-0 ${pathname.includes("Signup") ? "active" : "text-danger"}`}> Signup  </Link><br /></>}
       {links.includes("Profile") && <><Link to={`/Kambaz/Account/Profile`}
         className={`list-group-item border border-0 ${pathname.includes("Profile") ? "active" : "text-danger"}`}> Profile </Link><br /></>}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link to={`/Kambaz/Account/Users`} className={`list-group-item border border-0 ${active("Users")}`}> Users </Link>)}
     </div>
   );
 }
