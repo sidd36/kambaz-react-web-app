e0445180fa3d12a212cda1db6cad2cfad2b7e9ff
siddharth_sanjeev <sanjeev.si@northeastern.edu>
2025-04-16 21:07:17 -0400

    Hw6 commit 3

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -29,7 +29,7 @@ export const deleteUser = async (userId: string) => {
 };
 
 export const createUser = async (user: any) => {
-    const response = await axios.post(`${USERS_API}`, user);
+    const response = await axiosWithCredentials.post(`${USERS_API}`, user);
     return response.data;
 };
 
